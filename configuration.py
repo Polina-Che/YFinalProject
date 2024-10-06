f6e79bd81996fcb4d8bd4d4feb78a282993a9a88
Polina Chernova <polina-che369@yandex.ru>
2024-10-06 19:07:30 +0100

    Финальный проект яндекс практикум

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://0a679ff2-044e-49ce-bbfc-d1ed7b0ce013.serverhub.praktikum-services.ru"  # адрес сервера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="   # получить информацию о заказе по его номеру
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Полина",
+    "lastName": "Чернова",
+    "address": "Калнина 33",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 2,
+    "deliveryDate": "2023-06-02",
+    "comment": "Весь день дома",
+    "color": [
+        "BLACK"
+    ]
+}
